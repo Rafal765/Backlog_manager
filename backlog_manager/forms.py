0596e2c15eb4c0b1d78e4d948bca476c2beaad03
Rafal765 <r_kozak@tlen.pl>
2021-03-18 22:27:56 +0100

    Added documentation to forms.py

--- backlog_manager/forms.py
+++ backlog_manager/forms.py
@@ -6,6 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
+    """
+    Form for creating new user.
+    """
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
