3ade4026d2d61df87c46233a929539b5a624a09b
Rafal765 <r_kozak@tlen.pl>
2021-03-27 21:36:42 +0100

    Disabled Heroku-specific settings to prevent problems with tests

--- Projekt_koncowy/settings.py
+++ Projekt_koncowy/settings.py
@@ -126,5 +126,7 @@ except ImportError:
 
 # Configure Django App for Heroku.
 
-import django_on_heroku
-django_on_heroku.settings(locals())
+#import django_on_heroku
+#django_on_heroku.settings(locals())
+
+
